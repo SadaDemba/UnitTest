8c60806b782cf611b33842ec4928a9c1f9bfceef
SadaDemba <sadadembat9@gmail.com>
2024-05-02 12:48:42 +0200

    First commit

---
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -0,0 +1,12 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProviderController {
+    @GetMapping("/providers")
+    public String provider() {
+        return "providers.html";
+    }
+}
